f4373b1d5d1ee5a2d13eb7c0302ab202067d9c2d
rifqisec <rifqisec@gmail.com>
2017-03-21 15:56:04 +0700

    add password config database

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'danautoba');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '4KqjzTsDVDh1');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
